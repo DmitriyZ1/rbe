85833e180665893f21126399e89eb7b117488ecc
Dmitriy <zavoronk@mail.ru>
2023-05-26 22:06:05 +0300

    zal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+
 function App() {
+
+  const [out, setOut] = useState({name: '', age: ''});
+
+  const recl = (e) => {
+    if(e.target.id === 'userName'){
+      setOut({...out, name: e.target.value})
+    } else if (e.target.id === 'userAge') {
+      setOut({...out, age: e.target.value})
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <label htmlFor="userName">Имя </label> <input type="text" id="userName" value={out.name} onChange={recl}/>
+        <hr/>
+        <label htmlFor="userAge">Возраст </label> <input type="number" id="userAge" value={out.age} onChange={recl}/>
+        <h3>{`Имя:${out.name} Возрвст: ${out.age}`}</h3>
     </div>
   );
 }
